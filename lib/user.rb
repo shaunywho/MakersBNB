45610cbdb16abb682560e323f6fe7e55d6b00f90
Arsenii Gabov <agabov38@gmail.com>
2022-07-18 15:53:31 +0100

    user class

--- lib/user.rb
+++ lib/user.rb
@@ -1,2 +1,10 @@
 class User 
+    attr_accessor :id, :name, :email, :password
+
+  def initialize(id:, name:, email:, password:)
+    @id = id
+    @name = name
+    @email = email
+    @password = password
+  end
 end
