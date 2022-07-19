60dc607bd68ec9a88d7be412809010287e3dd365
delphine <delphine.mubiligi@gmail.com>
2022-07-19 10:22:12 +0100

    property model and repository classes

--- lib/property_repository.rb
+++ lib/property_repository.rb
@@ -1,5 +1,53 @@
 require_relative './property'
 
 class PropertyRepository
+  def find(id)
+    sql = 'SELECT * FROM properties WHERE id = $1;'
+    result_set = DatabaseConnection.exec_params(sql, [id])
+
+    return get_properties(result_set)[0]
+  end
+
+  def all
+    sql = 'SELECT * FROM properties;'
+    result_set = DatabaseConnection.exec_params(sql, [])
+
+    return get_properties(result_set)
+  end
+
+  def create(property)
+    sql = 'INSERT INTO properties 
+          (name, location, description, price, user_id) 
+          VALUES ($1, $2, $3, $4, $5);'
+    params = [property.name,
+    property.location,
+    property.description,
+    property.price,
+    property.user_id]
+    DatabaseConnection.exec_params(sql, params)
+  end
+
+  def my_properties(user_id)
+    sql = 'SELECT * FROM properties WHERE user_id = $1;'
+    result_set = DatabaseConnection.exec_params(sql, [user_id])
     
-end 
+    return get_properties(result_set)
+  end
+  
+  private
+
+  def get_properties(result_set)
+    properties = []
+    result_set.each do |record|
+      property = Property.new
+      property.id = record['id'].to_i
+      property.name = record['name']
+      property.location = record['location']
+      property.description = record['description']
+      property.price = record['price'].to_f
+      property.user_id = record['user_id'].to_i
+      properties << property
+    end
+    return properties
+  end
+end
--- spec/property_repository_spec.rb
+++ spec/property_repository_spec.rb
@@ -1,2 +1,106 @@
 require 'property_repository'
 
+def reset_users_table
+  seed_sql = File.read('seeds/makers_bnb_seeds.sql')
+  connection = PG.connect({ host: '127.0.0.1', dbname: 'makersbnb' })
+  connection.exec(seed_sql)
+end
+
+def reset_propertiess_table
+  seed_sql = File.read('seeds/makers_bnb_seeds.sql')
+  connection = PG.connect({ host: '127.0.0.1', dbname: 'makersbnb' })
+  connection.exec(seed_sql)
+end
+
+def reset_requests_table
+  seed_sql = File.read('seeds/makers_bnb_seeds.sql')
+  connection = PG.connect({ host: '127.0.0.1', dbname: 'makersbnb' })
+  connection.exec(seed_sql)
+end
+  
+describe PropertyRepository do
+  before(:each) do
+    reset_users_table
+    reset_propertiess_table
+    reset_requests_table
+  end
+
+  it 'finds and returns one property using id' do
+    repo = PropertyRepository.new
+    properties = repo.find(1)
+
+    expect(properties.id).to eq 1
+    expect(properties.name).to eq 'house1'
+    expect(properties.location).to eq 'place1'
+    expect(properties.description).to eq 'description1'
+    expect(properties.price).to eq 9.99
+    expect(properties.user_id).to eq 1
+  end
+
+  it 'returns all properties' do
+    repo = PropertyRepository.new
+    properties = repo.all
+    expect(properties.length).to eq 5
+
+    expect(properties[0].id).to eq 1
+    expect(properties[0].name).to eq 'house1'
+    expect(properties[0].location).to eq 'place1'
+    expect(properties[0].description).to eq 'description1'
+    expect(properties[0].price).to eq 9.99
+    expect(properties[0].user_id).to eq 1
+
+    expect(properties[2].id).to eq 3
+    expect(properties[2].name).to eq 'house3'
+    expect(properties[2].location).to eq 'place3'
+    expect(properties[2].description).to eq 'description3'
+    expect(properties[2].price).to eq 13.99
+    expect(properties[2].user_id).to eq 3
+
+    expect(properties[4].id).to eq 5
+    expect(properties[4].name).to eq 'house5'
+    expect(properties[4].location).to eq 'place5'
+    expect(properties[4].description).to eq 'description5'
+    expect(properties[4].price).to eq 15.99
+    expect(properties[4].user_id).to eq 1
+  end
+
+  it 'creates a new property object' do
+    property = Property.new
+    property.name = 'house6'
+    property.location = 'place6'
+    property.description = 'description6'
+    property.price = 11.99
+    property.user_id = 4
+
+    repo = PropertyRepository.new
+    repo.create(property)
+    properties = repo.all
+    expect(properties.last.id).to eq 6
+    expect(properties.last.name).to eq 'house6'
+    expect(properties.last.location).to eq 'place6'
+    expect(properties.last.description).to eq 'description6'
+    expect(properties.last.price).to eq 11.99
+    expect(properties.last.user_id).to eq 4
+  end
+
+  it 'returns all properties belonging to a specific user' do
+    repo = PropertyRepository.new
+    properties = repo.my_properties(1)
+
+    expect(properties.length).to eq 2
+
+    expect(properties[0].id).to eq 1
+    expect(properties[0].name).to eq 'house1'
+    expect(properties[0].location).to eq 'place1'
+    expect(properties[0].description).to eq 'description1'
+    expect(properties[0].price).to eq 9.99
+    expect(properties[0].user_id).to eq 1
+
+    expect(properties[1].id).to eq 5
+    expect(properties[1].name).to eq 'house5'
+    expect(properties[1].location).to eq 'place5'
+    expect(properties[1].description).to eq 'description5'
+    expect(properties[1].price).to eq 15.99
+    expect(properties[1].user_id).to eq 1
+  end
+end
